bbfde12c11d051c4e2b25108fcdb1ffab2e50c46
MadsOverggard <nissum_10@hotmail.com>
2023-02-10 11:27:32 +0100

    Runnable tilføjet

--- src/Start.java
+++ src/Start.java
@@ -12,7 +12,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
 
-public class Start extends Application {
+public class Start extends Application implements Runnable {
     public void start(Stage stage) {
         stage.setTitle("Spil");
         stage.setResizable(false);
@@ -33,6 +33,7 @@ public class Start extends Application {
     private final int tileSize = artSize * scale;
     private final int screenWidth = tileSize * 16;
     private final int screenHight = tileSize * 12;
+    private final int FPS = 60;
 
     private void initContent(GridPane pane) {
         Canvas canvas = new Canvas(screenWidth, screenHight);
@@ -41,9 +42,46 @@ public class Start extends Application {
 
         Image image = new Image("/billede.jpeg");
 
-        graphicsContext.drawImage(image, 0, 0);
-
+        graphicsContext.drawImage(image, 10, 10);
 
         pane.add(canvas, 0,0);
+    }
+
+
+    @Override
+    public void run() {
+
+        int drawCount = 0;
+        double drawInterval = 1000000000./FPS; // 0.016666 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+
+        while (true) {
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                // 1 UPDATE: update information such as positions
+
+                // 2 DRAW: draw the new updates on screen
+
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+                //FPS Counter terminal
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+
+        }
     }
 }
