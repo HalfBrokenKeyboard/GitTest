78375cbac5ed0636d89f44c0d428066a4447968d
tayyi <tayyipbasara@gmail.com>
2023-02-10 10:38:02 +0100

    x

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
+import java.sql.SQLOutput;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hejsa Mads, Tayyip, Adam og Christian!");
+        System.out.println("Hej Lucas!!!!");
     }
+
 }
