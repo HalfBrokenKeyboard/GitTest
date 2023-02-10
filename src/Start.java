adb3fcb0709328e86a8a44c37dcf1368f9d7caad
MadsOverggard <nissum_10@hotmail.com>
2023-02-10 11:00:05 +0100

    Spil klasse tilføjet + billede

--- src/Start.java
+++ src/Start.java
@@ -1,8 +1,16 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 public class Start extends Application {
     public void start(Stage stage) {
@@ -20,18 +28,25 @@ public class Start extends Application {
         stage.show();
     }
 
-    private int artSize = 16;
-    private int scale = 3;
-    private int tileSize = artSize * scale;
-    private int screenWidth = tileSize * 16;
-    private int screenHight = tileSize * 12;
+    private final int artSize = 16;
+    private final int scale = 3;
+    private final int tileSize = artSize * scale;
+    private final int screenWidth = tileSize * 16;
+    private final int screenHight = tileSize * 12;
 
     private void initContent(GridPane pane) {
-
-
         Canvas canvas = new Canvas(screenWidth, screenHight);
 
-        pane.add(canvas, 0,0);
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
+
+        Image image = new Image("/billede.jpeg");
+
+
+
+        graphicsContext.drawImage(image, 0, 0);
+
+
+        pane.add(canvas, 0,0);
     }
 }
