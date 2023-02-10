3c5dc8779e577bba4c9ba4337771800d030b78ca
Christiansurma <chrsurma@yahoo.dk>
2023-02-10 10:41:24 +0100

    PP

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hejsa Mads, Tayyip, Adam og Christian!");
         System.out.println("Hej Lucas!!!!");
+        System.out.println("Hvor er Nick?!?!?");
     }
 
 }
